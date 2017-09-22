09f1293589816368356cb2069928f0873a5d7f36
Linnea Honeker <linneah@email.arizona.edu>
2017-09-22 12:16:32 -0700

    all good

--- problems/unix-utils/cat-n.sh
+++ problems/unix-utils/cat-n.sh
@@ -2,7 +2,23 @@
 
 set -u
 
-FILE = $1
 
-if [[$# -lt 1]]; then
-	printf "Usage: %s 
+if [[ $# -lt 1 ]]; then
+	printf "Usage: %s file \n" $(basename $0)
+	exit 1
+fi
+
+FILE=$1
+
+if [[ ! -e "$FILE" ]]; then
+	echo "\"$FILE\"" is not a file
+	exit 1
+fi
+
+i=0
+
+while read -r LINE; do
+	let i++
+	echo $i $LINE 
+done < "$FILE"
+
--- problems/unix-utils/head.sh
+++ problems/unix-utils/head.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "OK"
+set -u
+
+if [[ $# -lt 1 ]]; then
+	printf "Usage: %s FILE [NUM]\n" $(basename $0)
+	exit 1
+fi
+
+FILE=$1
+HEAD=${2:-3}
+
+if [[ ! -e "$FILE" ]]; then
+	echo "\"$FILE\"" is not a file
+	exit 1
+fi
+
+i=0
+while [ $i -lt $HEAD ] && read -r LINE ; do
+		let i++
+		printf "%s\n" "$LINE"
+done < "$FILE"
