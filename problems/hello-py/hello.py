9bf9611d235104d13360b39b98509f9734b0313f
Linnea Honeker <linneah@email.arizona.edu>
2017-10-05 11:55:26 -0700

    added

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
-"""say hello to the arguments"""
 
-print('OK')
+import sys
+import os
+
+def main():
+	"""main"""
+	args= sys.argv
+
+	x=len(args)
+
+
+	if x < 2:
+		script = os.path.basename(args[0])
+		print('Usage: {} NAME [NAME2 ...]'.format(script))
+		sys.exit(1)
+	elif x == 2:
+		print('Hello to the 1 of you: {}!'.format(args[1]))
+	elif x == 3:
+		print('Hello to the 2 of you: {}!'.format(' and '.join(args[1:x])))
+
+	else:
+		
+		print('Hello to the {} of you: {}, and {}!'.format(x-1,', '.join(args[1:x-1]), args[x-1]))
+
+if __name__=='__main__':
+	main()
+
+
--- problems/hello-py/vowel_counter.py
+++ problems/hello-py/vowel_counter.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
-"""count the vowels in a word"""
 
-print('OK')
+import sys
+import os
+
+def main():
+	"""main"""
+	args= sys.argv
+
+	if len(args) < 2:
+                script = os.path.basename(args[0])
+                print('Usage: {} STRING'.format(script))
+                sys.exit(1)
+	
+	count_a, count_e, count_i, count_o, count_u = 0,0,0,0,0
+	
+	for letter in args[1]:
+		if letter == 'a' or letter == 'A':
+			count_a += 1
+		elif letter == 'e' or letter == 'E':
+			count_e += 1
+		elif letter == 'i' or letter == 'I':
+			count_i += 1
+		elif letter == 'o' or letter == 'O':
+			count_o += 1
+		elif letter == 'u' or letter == 'U':
+			count_u += 1
+	count = count_a + count_e + count_i + count_o + count_u
+	
+	if count == 1:
+		print('There is {} vowel in "{}."'.format(count,args[1]))
+	else:
+		print('There are {} vowels in "{}."'.format(count,args[1]))
+
+if __name__=='__main__':
+	main()
+
